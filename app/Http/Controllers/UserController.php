1c890c95aa88da0483dad51b1f151e1fe71b2809
AdrianRamadhan <adrianramadhan881@gmail.com>
2023-09-09 23:19:57 +0700

    add: login-user-api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
@@ -11,6 +12,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -33,5 +35,26 @@ class UserController extends Controller
         $user->save();
 
         return (new UserResource($user))->response()->setStatusCode(201);
+    }
+
+    public function login(UserLoginRequest $request): UserResource
+    {
+        $data = $request->validated();
+
+        $user = User::where('username', $data['username'])->first();
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "message" => [
+                        "username or password is wrong"
+                    ]
+                    ]
+            ], 401));
+        }
+
+        $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return new UserResource($user);
     }
 }
